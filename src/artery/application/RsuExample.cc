bbc666e10afbc7330fb59635fd1a5807d23f8d5e
ZorbMax <orban.maxime@gmail.com>
2024-01-31 11:40:53 +0100

    First try on custom services

--- src/artery/application/RsuExample.cc
+++ src/artery/application/RsuExample.cc
@@ -13,7 +13,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#include "ExampleService.h"
+#include "RsuExample.h"
 #include "artery/traci/VehicleController.h"
 #include <omnetpp/cpacket.h>
 #include <vanetza/btp/data_request.hpp>
@@ -26,67 +26,64 @@ using namespace vanetza;
 namespace artery
 {
 
-static const simsignal_t scSignalCamReceived = cComponent::registerSignal("CamReceived");
+Define_Module(RsuExample)
 
-Define_Module(ExampleService)
-
-ExampleService::ExampleService()
+RsuExample::RsuExample()
 {
 }
 
-ExampleService::~ExampleService()
+RsuExample::~RsuExample()
 {
 	cancelAndDelete(m_self_msg);
 }
 
-void ExampleService::indicate(const btp::DataIndication& ind, cPacket* packet, const NetworkInterface& net)
+void RsuExample::indicate(const btp::DataIndication& ind, cPacket* packet, const NetworkInterface& net)
 {
 	Enter_Method("indicate");
-	
+	std::cout << "RSU received\n";
 
-	if (packet->getByteLength() == 42) {	
-		std::cout << "Vehicle received : " << *packet << "\n";
+	if (packet->getByteLength() == 42) {
 		EV_INFO << "packet indication on channel " << net.channel << "\n";
+		std::cout << "RSU received : " << *packet << "\n";
 	}
 
 	delete(packet);
 }
 
-void ExampleService::initialize()
+void RsuExample::initialize()
 {
 	ItsG5Service::initialize();
-	m_self_msg = new cMessage("Example Service");
-	subscribe(scSignalCamReceived);
+	m_self_msg = new cMessage("RSU Example Service");
 
 	scheduleAt(simTime() + 3, m_self_msg);
 }
 
-void ExampleService::finish()
+void RsuExample::finish()
 {
 	// you could record some scalars at this point
 	ItsG5Service::finish();
 }
 
-void ExampleService::handleMessage(cMessage* msg)
+void RsuExample::handleMessage(cMessage* msg)
 {
 	Enter_Method("handleMessage");
 
 	if (msg == m_self_msg) {
-		std::cout << "Self message\n";
 		EV_INFO << "self message\n";
 	}
 }
 
-void ExampleService::trigger()
+
+void RsuExample::trigger()
 {
 	Enter_Method("trigger");
 
-	// use an ITS-AID reserved for testing purposes
+	/// use an ITS-AID reserved for testing purposes
 	static const vanetza::ItsAid example_its_aid = 16480;
-	std::cout << "Vehicle sending\n";
 
 	auto& mco = getFacilities().get_const<MultiChannelPolicy>();
 	auto& networks = getFacilities().get_const<NetworkInterfaceTable>();
+	std::cout << "RSU sending\n";
 
 	for (auto channel : mco.allChannels(example_its_aid)) {
 		auto network = networks.select(channel);
@@ -95,12 +92,12 @@ void ExampleService::trigger()
 			// use same port number as configured for listening on this channel
 			req.destination_port = host_cast(getPortNumber(channel));
 			req.gn.transport_type = geonet::TransportType::SHB;
-			req.gn.traffic_class.tc_id(static_cast<unsigned>(dcc::Profile::DP3));
+			req.gn.traffic_class.tc_id(static_cast<unsigned>(dcc::Profile::DP2));
 			req.gn.communication_profile = geonet::CommunicationProfile::ITS_G5;
 			req.gn.its_aid = example_its_aid;
-			std::cout << "Vehicle send one\n";
+			std::cout << "RSU sending\n";
 
-			cPacket* packet = new cPacket("Vehicle send packet");
+			cPacket* packet = new cPacket("RSU send packet");
 			packet->setByteLength(42);
 
 			// send packet on specific network interface
@@ -111,14 +108,9 @@ void ExampleService::trigger()
 	}
 }
 
-void ExampleService::receiveSignal(cComponent* source, simsignal_t signal, cObject*, cObject*)
+void RsuExample::receiveSignal(cComponent* source, simsignal_t signal, cObject*, cObject*)
 {
 	Enter_Method("receiveSignal");
-
-	if (signal == scSignalCamReceived) {
-		auto& vehicle = getFacilities().get_const<traci::VehicleController>();
-		EV_INFO << "Vehicle " << vehicle.getVehicleId() << " received a CAM in sibling serivce\n";
-	}
 }
 
 } // namespace artery
